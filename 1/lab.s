fe57c370d8234beaf0fd64265094c26330d8fa6c
Kirill <kirill.a.gribov@gmail.com>
2025-03-04 11:30:23 +0300

    написал решенеие

--- 1/lab.s
+++ 1/lab.s
@@ -1,47 +1,73 @@
 bits    64
-;       res=(a+b)*d*e/c-(c+d)/a
+;       res=a*(e-b)*c/(e+d)-(d+b)/e
 section .data
 res:
         dq      0
 a:
-        dw      40
+        dd      40
 b:
-        dw      100
+        dw      32767
 c:
-        dw      20
+        dd      2
 d:
-        dw      20
+        dw      32767
 e:
-        dw      30
+        dd      30
 section .text
 global  _start
 _start:
-        movsx   ebx, word[a]
-        or      ebx, ebx
-        jz      err
-        movsx   ecx, word[c]
-        jecxz   err
-        movsx   edi, word[d]
-        movsx   eax, word[e]
-        imul    edi
-        idiv    ecx
-        movsx   esi, word[b]
-        add     esi, ebx
-        imul    esi
-        mov     esi, eax
-        sal     rdx, 32
-        or      rsi, rdx
-        mov     eax, ecx
-        add     eax, edi
-        cdq
-        idiv    ebx
-        cdqe
-        sub     rsi, rax
-        mov     [res], rsi
+        mov     eax, dword [e]       
+        movsx   ebx, word [b]        
+        sub     eax, ebx   
+
+        mov     ecx, dword [a]      
+        imul    ecx      
+        
+        jo      overflow_error
+        
+        mov     ecx, dword [c]       
+        imul    ecx              
+        
+        jo      overflow_error
+        
+        mov     ecx, dword [e]       
+        movsx   esi, word [d]        
+        add     ecx, esi            
+        
+        or      ecx, ecx
+        jz      div_zero_error                 
+        
+        cdq                     
+        idiv    ecx                
+        
+        mov     esi, eax           
+        
+        movsx   eax, word [d]        
+        movsx   ebx, word [b]        
+        add     eax, ebx            
+        cdq                         
+        
+        mov     ecx, dword [e]       
+        or      ecx, ecx
+        jz      div_zero_error                 
+        
+        idiv    ecx                 
+        
+        sub     esi, eax            
+               
+        movsx   rsi, esi            
+        mov     qword [res], rsi     
+        
         mov     eax, 60
-        mov     edi, 0
+        xor     edi, edi
         syscall
-err:
+        
+div_zero_error:
         mov     eax, 60
         mov     edi, 1
+        syscall
+
+overflow_error:
+        mov     eax, 60
+        mov     edi, 2
         syscall
