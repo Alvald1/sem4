5905b02e8610165b604b3ed277b629aed8d51959
Kirill <kirill.a.gribov@gmail.com>
2025-03-18 13:53:16 +0300

    плсчет начальных индексов

--- 2/lab.asm
+++ 2/lab.asm
@@ -16,18 +16,31 @@ global  _start
 _start: 
 ; r8 - кол-во диагоналей
 	movsx r8, byte[size]
-	imul  r8, 2
-	sub   r8, 1
+	shl   r8, 1          ; r8 * 2	
+	dec   r8             ; r8 - 1
+
 ; (matrix * i) + j
 
 	mov rcx, r8
+	
+	mov   r14, 0          ; начальный i
+	movsx r15, byte[size] ; начальный j
+	dec   r15
 
 loop_1: ; итерация по кол-ву диагоналей
-	mov r8, rcx
+	mov r8, rcx ; созраняем счетчик
 	
-	test rax, rax ;заглушка
 
-	mov  rcx, r8
+	; пересчет начальный индексов
+	test r15, r15 ; если j != 0: j - 1
+	jnz  L1
+	dec  r15
+L1:
+	inc r14 ; если j = 0 : i + 1
+
+	
+
+	mov  rcx, r8 ; выгружаем счетчик
 	loop loop_1
 
 
