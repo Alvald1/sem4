237d1c3ef93ad242fbbefcfce0f57d84c76d26de
Kirill <kirill.a.gribov@gmail.com>
2025-03-25 21:12:53 +0300

    написал вычисление адреса по индексу в диагонали

--- 2/lab.asm
+++ 2/lab.asm
@@ -3,7 +3,10 @@ bits    64
 section .data
 	
 size   db 4
-matrix db 5, 6, 7, 1, 15, 13, 14, 10, 16, 2, 3, 11, 12, 4, 8, 9
+matrix db 5, 6, 7, 1, 1, \
+          5, 13, 14, 10, \
+		  16, 2, 3, 11,  \
+		  12, 4, 8, 9
 
 ; 5		6	7	1
 ; 15	13 	14 	10
@@ -14,6 +17,7 @@ matrix db 5, 6, 7, 1, 15, 13, 14, 10, 16, 2, 3, 11, 12, 4, 8, 9
 ; 20	17 	17 	12
 ; 22 	7 	7 	14
 ; 19 	10 	13 	13
+
 	
 section .text
 global  _start
@@ -22,7 +26,7 @@ _start:
 	movsx r12, byte[size] ; size
 	dec   r12
 
-	mov rsi, [matrix] ; указатель на первый элемент
+	mov rbx, [matrix] ; указатель на первый элемент
 
 ; r13 - кол-во диагоналей
 	mov r13, r12
@@ -36,17 +40,12 @@ _start:
 	mov r14, 0   ; начальный i
 	mov r15, r12 ; начальный j
 
-	mov r10, 0 ; для тестов
 
 loop_1: ; итерация по кол-ву диагоналей
 	push rcx ; созраняем счетчик
 	
-	mov r8, r14
-	mov r9, r15
-
-	inc r10
-
-	call line_traversal
+	mov  rdi, 2
+	call calculate_address
 
 	; пересчет начальный индексов
 	test r15, r15 ; если j != 0: j - 1
@@ -57,7 +56,6 @@ L1:
 	inc r14 ; если j = 0 : i + 1
 L2:
 
-
 	pop  rcx    ; выгружаем счетчик
 	loop loop_1
 
@@ -66,26 +64,43 @@ L2:
 	mov edi, 0
 	syscall
 
-line_traversal:
-L4:
-	mov  rdx, r12 ; размер
-	inc  rdx      ; привести к настоящему размеру
-	imul rdx, r8  ; сдвиг на строку
-	add  rdx, r9  ; сдвиг на столбец
 
-	add matrix[rdx], r10
-
-	cmp r8, r12 ; i <=> size - 1
-	je  L3
-
-	cmp r9, r12 ; j <=> size - 1
-	je  L3
+calculate_address:
+	; r12 - (size - 1)
+	; r14 - i база
+	; r15 - j база
+	; rbx - база
+	; rdi - индекс
 	
-	inc r8 ; i++
-	inc r9 ; j++
+	mov r8, r14
+	add r8, rdi ; i + индекс
 
-	jmp L4
+	mov r9, r15
+	add r9, rdi ; j + индекс
 
-L3:
+	cmp r8, r12
+	jng L5      ; i + индекс <= (size - 1) 	
+
+	mov rax, 0
 	ret
 
+L5:
+	cmp r9, r12
+	jng L6      ; j + индекс <= (size - 1)
+
+	mov rax, 0
+	ret
+
+L6: 
+
+	mov rax, r8
+
+	mov rdx, r12
+	inc rdx      ; size
+
+	imul rax, rdx ; i * size
+	add  rax, r9  ; (i * size) + j
+
+	add rax, rbx ; (i * size) + j + база 
+
+	ret
