e64b673028a03ebdf311b21c256ce1f22ce617fe
Kirill <kirill.a.gribov@gmail.com>
2025-04-03 00:23:52 +0300

    переписал на 32 бита и исправил баги

--- 2/lab.asm
+++ 2/lab.asm
@@ -6,8 +6,8 @@ section .data
 %endif
 	
 sort_order db SORT_ORDER
-size       db 4
-matrix     db 5, 6, 7, 1,  \
+size       dd 4
+matrix     dd 5, 6, 7, 1,  \
           5, 13, 14, 10, \
 		  16, 2, 3, 11,  \
 		  12, 4, 8, 9
@@ -31,36 +31,36 @@ section .text
 global  _start
 	
 _start: 
-	movsx r12, byte[size] ; size
-	dec   r12
+	mov r12d, [size] ; size
+	dec r12d
 
-	lea rbx, [matrix] ; указатель на первый элемент
+	lea ebx, [matrix] ; указатель на первый элемент
 
-; r13 - кол-во диагоналей
-	mov r13, r12
-	shl r13, 1   ; r13 * 2	
-	inc r13      ; r13 + 1
+; r13d - кол-во диагоналей
+	mov r13d, r12d
+	shl r13d, 1    ; r13d * 2	
+	inc r13d       ; r13d + 1
 
 ; (matrix * i) + j
 
-	mov rcx, r13
+	mov ecx, r13d
 	
-	mov r14, 0   ; начальный i
-	mov r15, r12 ; начальный j
+	mov r14d, 0    ; начальный i
+	mov r15d, r12d ; начальный j
 
 
 loop_1: ; итерация по кол-ву диагоналей
 	push rcx         ; сохраняем счетчик	
-	call insert_sort ; r8, r9, r10, r11, rcx, rdx, rbp - используются
+	call insert_sort ; r8d, r9d, r10d, r11d, ecx, edx, ebp - используются
 	pop  rcx         ; выгружаем счетчик
 
 	; пересчет начальный индексов
-	test r15, r15 ; если j != 0: j - 1
+	test r15d, r15d ; если j != 0: j - 1
 	jz   L1
-	dec  r15
+	dec  r15d
 	jmp  L2
 L1:
-	inc r14 ; если j = 0 : i + 1
+	inc r14d ; если j = 0 : i + 1
 L2:
 
 	loop loop_1
@@ -71,221 +71,234 @@ L2:
 	syscall
 
 
-calculate_address: ; r8, r9,
-	; r12 - (size - 1)
-	; r14 - i база
-	; r15 - j база
-	; rbx - база
-	; rdi - индекс
+calculate_address: ; r8d, r9d, edx
+	; r12d - (size - 1)
+	; r14d - i база
+	; r15d - j база
+	; ebx - база
+	; edi - индекс
 	
-	mov r8, r14
-	add r8, rdi ; i + индекс
+	mov r8d, r14d
+	add r8d, edi  ; i + индекс
 
-	mov r9, r15
-	add r9, rdi ; j + индекс
+	mov r9d, r15d
+	add r9d, edi  ; j + индекс
 
-	cmp r8, r12
-	jng L5      ; i + индекс <= (size - 1) 	
+	cmp r8d, r12d
+	jng L5        ; i + индекс <= (size - 1) 	
 
-	mov rax, 0
+	mov eax, 0
 	ret
 
 L5:
-	cmp r9, r12
-	jng L6      ; j + индекс <= (size - 1)
+	cmp r9d, r12d
+	jng L6        ; j + индекс <= (size - 1)
 
-	mov rax, 0
+	mov eax, 0
 	ret
 
 L6: 
 
-	mov rax, r8
+	mov eax, r8d
 
-	mov rdx, r12
-	inc rdx      ; size
+	mov edx, r12d
+	inc edx       ; size
 
-	imul rax, rdx ; i * size
-	add  rax, r9  ; (i * size) + j
+	imul eax, edx ; i * size
+	add  eax, r9d ; (i * size) + j
 
-	add rax, rbx ; (i * size) + j + база 
+	lea eax, [ebx + eax * 4] ; (i * size) + j + база 
 
 	ret
 
 
-bin_search: ; rcx, r8, r9, 
-	; r12 - (size - 1)
-	; r14 - i база
-	; r15 - j база
-	; rbx - база
+bin_search: ; ecx, r8d, r9d, 
+	; r12d - (size - 1)
+	; r14d - i база
+	; r15d - j база
+	; ebx - база
 
-	; rdi - item
-	; r8 - low
-	; r9 - high
+	; edi - item
+	; r8d - low
+	; r9d - high
 loop_2:
-	cmp r8, r9
-	jge L7     ; low >= high
+	cmp r8d, r9d
+	jge L7       ; low >= high
 
-	mov rcx, r9 ; high
-	sub rcx, r8 ; high - low
-	sar rcx, 1  ; (high - low) / 2
-	add rcx, r8 ; low + (high - low) / 2 == mid
+	mov ecx, r9d ; high
+	sub ecx, r8d ; high - low
+	sar ecx, 1   ; (high - low) / 2
+	add ecx, r8d ; low + (high - low) / 2 == mid
 
 	push rdi
 	push r8
 	push r9
-	mov  rdi, rcx          ; mid
+	mov  edi, ecx          ; mid
+	push rdx
 	call calculate_address
+	pop  rdx
 	pop  r9
 	pop  r8
 	pop  rdi
 
-	test rax, rax
-	jz   buffer_overflow ; rax == 0
+	test eax, eax
+	jz   buffer_overflow ; eax == 0
 
 
-	cmp [rax], rdi
+	cmp [eax], edi
 	jne L8         ; arr[mid] != item
-	inc rcx        ; mid + 1
-	mov rax,   rcx
+	inc ecx        ; mid + 1
+	mov eax,   ecx
 	ret
 
 L8:
 	cmp byte [sort_order], 0
 	jne descending_order_1
-	
+
+	cmp [eax], edi
 	; Ascending order logic
-	jg  L9      ; arr[mid] > item
-	inc rcx     ; mid + 1
-	mov r8, rcx ; low = mid + 1
+	jg  L9         ; arr[mid] > item
+	inc ecx        ; mid + 1
+	mov r8d,   ecx ; low = mid + 1
 	jmp loop_2
 
 descending_order_1:
+
+	cmp [eax], edi
 	; Descending order logic
-	jl  L9      ; arr[mid] < item (for descending order)
-	inc rcx     ; mid + 1
-	mov r8, rcx ; low = mid + 1
+	jl  L9         ; arr[mid] < item (for descending order)
+	inc ecx        ; mid + 1
+	mov r8d,   ecx ; low = mid + 1
 	jmp loop_2
 
 L9:
-	dec rcx     ; mid - 1
-	mov r9, rcx ; high = mid - 1
+	dec ecx      ; mid - 1
+	mov r9d, ecx ; high = mid - 1
 	jmp loop_2
 
 L7:
 	push rdi
 	push r8
 	push r9
-	mov  rdi, r8           ; low
+	mov  edi, r8d          ; low
+	push rdx
 	call calculate_address
+	pop  rdx
 	pop  r9
 	pop  r8
 	pop  rdi
 
-	test rax, rax
-	jz   buffer_overflow ; rax == 0
+	test eax, eax
+	jz   buffer_overflow ; eax == 0
 
 	cmp byte [sort_order], 0
 	jne descending_order_2
 	
 	; Ascending order logic
-	cmp rdi, [rax]
-	mov rax, r8    ; low
+	cmp edi, [eax]
+	mov eax, r8d   ; low
 	jng L10        ; item <= arr[low]	
-	inc rax        ; low + 1
+	inc eax        ; low + 1
 	ret
 
 descending_order_2:
 	; Descending order logic
-	cmp rdi, [rax]
-	mov rax, r8    ; low
+	cmp edi, [eax]
+	mov eax, r8d   ; low
 	jnl L10        ; item >= arr[low] (for descending order)
-	inc rax        ; low + 1
+	inc eax        ; low + 1
 	ret
 
 L10:
 	ret
 
 calc_diag_len: 
-	; r12 - (size - 1)
-	; r14 - i база
-	; r15 - j база
+	; r12d - (size - 1)
+	; r14d - i база
+	; r15d - j база
 	
-	mov rax, r12
-	cmp r14, r15
-	jg  L11      ; i > j
-	sub rax, r15
+	mov eax,  r12d
+	cmp r14d, r15d
+	jg  L11        ; i > j
+	sub eax,  r15d
 	jmp L12
 
 L11:
-	sub rax, r14
+	sub eax, r14d
 L12:
-	inc rax
+	inc eax
 	ret
 
 
-insert_sort: ; r8, r9, r10, r11, rcx, rdx, rbp
-	; r12 - (size - 1)
-	; r14 - i база
-	; r15 - j база
-	; rbx - база
+insert_sort: ; r8d, r9d, r10d, r11d, ecx, edx, ebp
+	; r12d - (size - 1)
+	; r14d - i база
+	; r15d - j база
+	; ebx - база
 
 	call calc_diag_len
-	mov  r10, rax      ; diag len
-	mov  r8,  1        ; i
+	mov  r10d, eax     ; diag len
+	mov  r8d,  1       ; i
 
 loop_3:
-	cmp r8, r10
+	cmp r8d, r10d
 	je  L13
 
-	mov r9, r8 ; j = i
-	dec r9     ; j--
+	mov r9d, r8d ; j = i
+	dec r9d      ; j--
 
-	mov  rdi, r8           ; i
+	mov  edi, r8d          ; i
 	push r8
 	push r9
+	push rdx
 	call calculate_address
+	pop  rdx
 	pop  r9
 	pop  r8
 
-	mov r11, [rax] ; arr[i] == selected
+	mov r11d, [eax] ; arr[i] == selected
 
 	push r8
 	push r9
 	push rcx
-	mov  rdi, r11
-	mov  r8,  0
+	mov  edi, r11d
+	mov  r8d, 0
 	call bin_search ; low = 0, high = j
 	pop  rcx
 	pop  r9
 	pop  r8
 
-	mov rdx, rax ; location
+	mov edx, eax ; location
 
 loop_4:
-	cmp r9, rdx
-	jl  L14     ; j < location
+	cmp r9d, edx
+	jl  L14      ; j < location
 	
 	push r8
 	push r9
-	mov  rdi, r9           ; j
-	inc  rdi               ; j + 1
+	mov  edi, r9d          ; j
+	inc  edi               ; j + 1
+	push rdx
 	call calculate_address
+	pop  rdx
 	pop  r9
 	pop  r8
 	
-	mov rcx, rax ; arr[j + 1]
+	mov ecx, eax ; arr[j + 1]
 
 	push r8
 	push r9
-	mov  rdi, r9           ; j
+	mov  edi, r9d          ; j
+	push rdx
 	call calculate_address
+	pop  rdx
 	pop  r9
 	pop  r8
 
-	mov rbp,   [rax] ; arr[j]
-	mov [rcx], rbp   ; arr[j + 1] = arr[j]
+	mov ebp,   [eax] ; arr[j]
+	mov [ecx], ebp   ; arr[j + 1] = arr[j]
 
-	dec r9 ; j--
+	dec r9d ; j--
 
 	jmp loop_4
 
@@ -293,15 +306,17 @@ L14:
 
 	push r8
 	push r9
-	mov  rdi, r9           ; j
-	inc  rdi               ; j + 1
+	mov  edi, r9d          ; j
+	inc  edi               ; j + 1
+	push rdx
 	call calculate_address
+	pop  rdx
 	pop  r9
 	pop  r8
 
-	mov [rax], r11
+	mov [eax], r11d
 
-	inc r8
+	inc r8d
 	jmp loop_3
 L13:
 	ret
